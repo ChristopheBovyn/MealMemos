8567ea11087c35e61c7dbc46216282c36291e228
Christophe <christophe.bovyn@orange.com>
2019-10-31 16:29:29 +0100

    Commit suite

--- iOS/LoginViewController.designer.cs
+++ iOS/LoginViewController.designer.cs
@@ -12,9 +12,47 @@ namespace MealMemos.iOS
 	[Register ("LoginViewController")]
 	partial class LoginViewController
 	{
+		[Outlet]
+		UIKit.UITextField confirmTextView { get; set; }
+
+		[Outlet]
+		UIKit.UITextField emailTextView { get; set; }
+
+		[Outlet]
+		UIKit.UIButton loginBtn { get; set; }
+
+		[Outlet]
+		UIKit.UITextField passTextView { get; set; }
+
+		[Outlet]
+		UIKit.UIButton resetBtn { get; set; }
 		
 		void ReleaseDesignerOutlets ()
 		{
+			if (loginBtn != null) {
+				loginBtn.Dispose ();
+				loginBtn = null;
+			}
+
+			if (resetBtn != null) {
+				resetBtn.Dispose ();
+				resetBtn = null;
+			}
+
+			if (confirmTextView != null) {
+				confirmTextView.Dispose ();
+				confirmTextView = null;
+			}
+
+			if (passTextView != null) {
+				passTextView.Dispose ();
+				passTextView = null;
+			}
+
+			if (emailTextView != null) {
+				emailTextView.Dispose ();
+				emailTextView = null;
+			}
 		}
 	}
 }
